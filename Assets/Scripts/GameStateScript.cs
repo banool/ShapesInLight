749c678a167a6798680f8ea2e92847a980fd48bd
Daniel Porteous <danielporteous1@gmail.com>
2017-07-02 23:29:54 +1000

    Initial commit

---
+++ Assets/Scripts/GameStateScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateScript : MonoBehaviour {
+
+	public Light firstLight;
+
+	List<Light> lights;
+
+	List<Vector3> defaultLocations;
+	List<Vector3> defaultRotations;
+		
+	// Use this for initialization
+	void Start () {
+		lights = new List<Light> ();
+
+		defaultLocations = new List<Vector3>();
+		defaultRotations = new List<Vector3>();
+		// Left
+		defaultLocations.Add(new Vector3(-2,2,0));
+		defaultRotations.Add(new Vector3(45,90,-45));
+		// Right
+		defaultLocations.Add(new Vector3(2,2,0));
+		defaultRotations.Add(new Vector3(45,270,45));
+		// Back
+		defaultLocations.Add(new Vector3(0,2,2));
+		defaultRotations.Add(new Vector3(135,0,0));
+
+		if (defaultLocations.Count != defaultRotations.Count)
+			throw new UnityException("# of locations and rotations not equal");
+	}
+
+	public void CreateLight () {
+		int target = lights.Count;
+		if (lights.Count >= defaultLocations.Count)
+			target = 0;
+		Light fresh = Instantiate (firstLight,defaultLocations [target],Quaternion.Euler(defaultRotations [target]));
+		fresh.gameObject.SetActive (true);
+		lights.Add(fresh);
+	}
+
+	public void DeleteLight () {
+		if (lights.Count == 0)
+			return;
+		Light target = lights [lights.Count - 1];
+		lights.RemoveAt (lights.Count-1);
+		Destroy (target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/HandleLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ToggleOnOff () {
+		bool curr = gameObject.GetComponent<Light> ().enabled;
+		gameObject.GetComponent<Light> ().enabled = !curr;
+	}
+}
