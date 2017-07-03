372fded1adb938a712c56fd2015952a47b4cc01a
Daniel Porteous <danielporteous1@gmail.com>
2017-07-03 13:12:44 +1000

    Switching between perspectives. Early mves, early mouse stuff

---
+++ Assets/Scripts/CameraMoveScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMoveScript : MonoBehaviour {
+
+	float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject active = this.gameObject;
+		if (Input.GetKey ("left") || Input.GetKey ("a"))
+			active.transform.localPosition -= active.transform.right * speed * Time.deltaTime;
+		if (Input.GetKey ("right") || Input.GetKey ("d"))
+			active.transform.localPosition += active.transform.right * speed * Time.deltaTime;
+		if (Input.GetKey ("up") || Input.GetKey ("w"))
+			active.transform.localPosition += active.transform.forward * speed * Time.deltaTime;
+		if (Input.GetKey ("down") || Input.GetKey ("s"))
+			active.transform.localPosition -= active.transform.forward * speed * Time.deltaTime;
+		if (Input.GetKey ("space"))
+			active.transform.localPosition += active.transform.up * speed * Time.deltaTime;
+		if (Input.GetKey ("left shift"))
+			active.transform.localPosition -= active.transform.up * speed * Time.deltaTime;
+	}
+}
+
--- Assets/Scripts/GameStateScript.cs
+++ Assets/Scripts/GameStateScript.cs
@@ -5,27 +5,37 @@ using UnityEngine;
 public class GameStateScript : MonoBehaviour {
 
 	public Light firstLight;
+	public Camera mainCamera;
 
 	List<Light> lights;
 
 	List<Vector3> defaultLocations;
 	List<Vector3> defaultRotations;
+
+	int currentPerspective = -1;
+
+	Vector3 cameraPosition;
+	Quaternion cameraRotation;
+
+	float speed = 1.0f;
 		
 	// Use this for initialization
 	void Start () {
 		lights = new List<Light> ();
+		cameraPosition = mainCamera.transform.position;
+		cameraRotation = mainCamera.transform.rotation;
 
 		defaultLocations = new List<Vector3>();
 		defaultRotations = new List<Vector3>();
 		// Left
 		defaultLocations.Add(new Vector3(-2,2,0));
-		defaultRotations.Add(new Vector3(45,90,-45));
+		defaultRotations.Add(new Vector3(45,90,0));
 		// Right
 		defaultLocations.Add(new Vector3(2,2,0));
-		defaultRotations.Add(new Vector3(45,270,45));
+		defaultRotations.Add(new Vector3(45,270,0));
 		// Back
 		defaultLocations.Add(new Vector3(0,2,2));
-		defaultRotations.Add(new Vector3(135,0,0));
+		defaultRotations.Add(new Vector3(135,0,-180));
 
 		if (defaultLocations.Count != defaultRotations.Count)
 			throw new UnityException("# of locations and rotations not equal");
@@ -46,10 +56,68 @@ public class GameStateScript : MonoBehaviour {
 		Light target = lights [lights.Count - 1];
 		lights.RemoveAt (lights.Count-1);
 		Destroy (target);
+		// GetNextPerspective() will push this back to -1.
+		currentPerspective = -2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		// Handle switching perspectives.
+		if (Input.GetKeyDown ("tab")) {
+			GetNextPerspective ();
+			print (currentPerspective);
+			if (currentPerspective == -1) {
+				mainCamera.gameObject.transform.SetPositionAndRotation (cameraPosition, cameraRotation);
+			} else {
+				if (currentPerspective == 0) {
+					cameraPosition = mainCamera.gameObject.transform.position;
+					cameraRotation = mainCamera.gameObject.transform.rotation;
+				}
+				mainCamera.gameObject.transform.SetPositionAndRotation (
+					lights [currentPerspective].gameObject.transform.position,
+					lights [currentPerspective].gameObject.transform.rotation);
+			}
+		}
+		// Handle movement
+		GameObject activeLight;
+		if (currentPerspective >= 0) {
+			activeLight = lights [currentPerspective].gameObject;
+			if (Input.GetKey ("left") || Input.GetKey ("a"))
+				activeLight.transform.localPosition -= activeLight.transform.right * speed * Time.deltaTime;
+			if (Input.GetKey ("right") || Input.GetKey ("d"))
+				activeLight.transform.localPosition += activeLight.transform.right * speed * Time.deltaTime;
+			if (Input.GetKey ("up") || Input.GetKey ("w"))
+				activeLight.transform.localPosition += activeLight.transform.forward * speed * Time.deltaTime;
+			if (Input.GetKey ("down") || Input.GetKey ("s"))
+				activeLight.transform.localPosition -= activeLight.transform.forward * speed * Time.deltaTime;
+			if (Input.GetKey ("space"))
+				activeLight.transform.localPosition += activeLight.transform.up * speed * Time.deltaTime;
+			if (Input.GetKey ("left shift"))
+				activeLight.transform.localPosition -= activeLight.transform.up * speed * Time.deltaTime;
+		}
 	}
+
+	void GetNextPerspective () {
+		// -1 means the main camera.
+		currentPerspective += 1;
+		if (currentPerspective >= lights.Count || currentPerspective < -1)
+			currentPerspective = -1;
+	}
+
+	private void moveForward(float speed) {
+		transform.localPosition += transform.forward * speed * Time.deltaTime;
+	}
+
+	private void moveBack(float speed) {
+		transform.localPosition -= transform.forward * speed * Time.deltaTime;
+	}
+
+	private void moveRight(float speed) {
+		transform.localPosition += transform.right * speed * Time.deltaTime;
+	}
+
+	private void moveLeft(float speed) {
+		transform.localPosition -= transform.right * speed * Time.deltaTime;
+	}
+
 }
